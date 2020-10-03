a943de946311c674d79f560cae85e22766e0d270
KWedwick <64446581+kwedwick@users.noreply.github.com>
2020-10-03 18:45:21 -0500

    updated service-worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,6 +1,9 @@
-const APP_PREFIX = 'BudgetTracker-';
-const VERSION = 'version_01';
-const CACHE_NAME = APP_PREFIX + VERSION;
+// const APP_PREFIX = 'BudgetTracker-';
+// const VERSION = 'version_01';
+// const CACHE_NAME = APP_PREFIX + VERSION;
+
+const CACHE_NAME = 'budget-tracker-cache-v1';
+const DATA_CACHE_NAME = 'data-cache-v1';
 
 const FILES_TO_CACHE = [
     "./index.html",
@@ -12,45 +15,66 @@ const FILES_TO_CACHE = [
 //installs files to cache
 self.addEventListener('install', function (e) {
     e.waitUntil(
-        caches.open(CACHE_NAME).then(function (cache) {
-            console.log('installing cache : ' + CACHE_NAME)
-            return cache.addAll(FILES_TO_CACHE)
+        caches.open(CACHE_NAME).then(cache => {
+            console.log('Your files were pre-cached successfully!');
+            return cache.addAll(FILES_TO_CACHE);
         })
-    )
+    );
+    self.skipWaiting();
 });
 
-self.addEventListener('activate', function (e) {
-    e.waitUntil(
-        caches.keys().then(function (keyList) {
-            let cacheKeeplist = keyList.filter(function (key) {
-                return key.indexOf(APP_PREFIX);
-            });
-            cacheKeeplist.push(CACHE_NAME);
+self.addEventListener('fetch', function (evt) {
+    if (evt.request.url.includes('/api/')) {
+        evt.respondWith(
+            caches
+                .open(DATA_CACHE_NAME)
+                .then(cache => {
+                    return fetch(evt.request)
+                        .then(response => {
+                            // If the response was good, clone it and store it in the cache.
+                            if (response.status === 200) {
+                                cache.put(evt.request.url, response.clone());
+                            }
 
-            return Promise.all( // if the files are present we delete the old ones
-                keyList.map(function (key, i) {
-                    if (cacheKeeplist.indexOf(key) === -1) {
-                        console.log('deleting cache : ' + keyList[i]);
-                        return caches.delete(keyList[i]);
-                    }
+                            return response;
+                        })
+                        .catch(err => {
+                            // Network request failed, try to get it from the cache.
+                            return cache.match(evt.request);
+                        });
                 })
-            );
+                .catch(err => console.log(err))
+        );
+        return;
+    }
+    evt.respondWith(
+        fetch(evt.request).catch(function () {
+            return caches.match(evt.request).then(function (response) {
+                if (response) {
+                    return response;
+                } else if (evt.request.headers.get('accept').includes('text/html')) {
+                    // return the cached home page for all requests for html pages
+                    return caches.match('/');
+                }
+            });
         })
     );
 });
 
-//determines if we fetch from cache or server
-self.addEventListener('fetch', function (e) {
-    console.log('fetch request: ' + e.request.url)
-    e.respondWith(
-        caches.match(e.request).then(function (request) {
-            if (request) {
-                console.log('responding with cache : ' + e.request.url)
-                return request
-            } else {
-                console.log('file is not cached, fetching : ' + e.request.url)
-                return fetch(e.request)
-            }
-        })
-    )
-});
+
+
+// //determines if we fetch from cache or server
+// self.addEventListener('fetch', function (e) {
+//     console.log('fetch request: ' + e.request.url)
+//     e.respondWith(
+//         caches.match(e.request).then(function (request) {
+//             if (request) {
+//                 console.log('responding with cache : ' + e.request.url)
+//                 return request
+//             } else {
+//                 console.log('file is not cached, fetching : ' + e.request.url)
+//                 return fetch(e.request)
+//             }
+//         })
+//     )
+// });
